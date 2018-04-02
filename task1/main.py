eb85dd9acd24fd9b3f8f42399959550b61710bcd
Francesco Saverio <francesco.saverio.varini@gmail.com>
2018-04-02 22:30:58 +0200

    Preprocessing implemented, creation of batches on the fly implemented and ready for training. Next: save model and preprocessing to load

--- task1/main.py
+++ task1/main.py
@@ -1,6 +1,8 @@
 import yaml
 import os
-import data_utilities as import_data
+import data_utilities as data_utilities
+import training_utils as train_utils
+import numpy as np
 
 def main():
     # load variables from config.yml
@@ -19,6 +21,11 @@ def main():
 
     """load data and preprocess it into vectorial embeddings"""
 
+    w2v_model_filename = "w2v_model"
+    dataset_filename = "input_data"
+    model_to_load = False
+
+
     emb_dim = config['embeddings_size']
     len_sentences = config['sentence_len']
     vocab_dim = config['vocabulary_size']
@@ -26,13 +33,28 @@ def main():
     end_placeholder = config['token']['eos']   
     pad_placeholder = config['token']['pad']   
     unk_placeholder = config['token']['unk']   
-    file_path = config['path']['data']
-    file_path = file_path+"/sentences.train"
+    data_folder_path = config['path']['data']
+    data_file_path = data_folder_path+"/sentences.train"
+    nb_batches_per_epoch = config['batches_per_epoch']
+    batch_size = config['batch_size']
 
-    utils = import_data.data_utils(emb_dim,len_sentences,vocab_dim,start_placeholder,end_placeholder,pad_placeholder,unk_placeholder)
-    input_data = utils.load_data(file_path)
-    #print(input_data)
+    """Create model and preprocess data, 
+       or load the saved model and the data preprocessed in a previous run"""
+    if not model_to_load:
+        utils = data_utilities.data_utils(emb_dim,len_sentences,vocab_dim,start_placeholder,end_placeholder,pad_placeholder,unk_placeholder)
+        model_w2v, dataset = utils.load_data(data_file_path)
+        #model_w2v.save(data_folder_path+"/"+w2v_model_filename)
+        #np.savetxt(data_folder_path+"/"+dataset_filename,dataset,newline="\n")
+    else:
+        model = Word2Vec.load(data_folder_path+"/"+w2v_model_filename)
+        dataset = np.loadtxt(data_folder_path+"/"+data_folder_path, delimiter="\n")
+        dataset = [x.strip("\n") for x in dataset]
+    
 
+    batches=train_utils.create_batches(nb_batches_per_epoch, batch_size, model_w2v, dataset, dataset_size)
+    print("Printing batches dimensions")
+    print(batches.shape)
+    
 
 
 main()

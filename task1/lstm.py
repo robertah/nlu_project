a4cbc90400e640c32ff709eabbb620523e0bdead
Nini <gamelani@ethz.ch>
2018-04-03 12:58:15 +0200

    Merge branches 'costanza' and 'master' of https://github.com/robertah/nlu_project into Mel
    # Conflicts:
    #	.gitignore
    #	task1/lstm.py

--- task1/lstm.py
+++ task1/lstm.py
@@ -1,1 +1,23 @@
 import tensorflow as tf
+import numpy as np
+
+# in progress by Mel --> feel free to modify
+
+#variables used:
+# batch_size, embeddings_size
+
+class lstm(object):
+
+    def __init__(self, batch_size, embeddings_size):
+        self.input_x = tf.placeholder(tf.float32,
+                                      shape=(batch_size, embeddings_size), name="input_x")
+
+        self.b = tf.Variable(tf.zeros((embeddings_size,)), name="b")
+        self.W = tf.Variable(tf.random_uniform((batch_size, embeddings_size), -1, 1), name="W")
+
+        self.C_t =
+
+        self.h_t =
+
+    def sigmoid(self, x):
+        return 1/(1 + np.exp(-x))

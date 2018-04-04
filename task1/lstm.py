3f3b53ee5f07280c141dcefa2241cd1aad435765
Nini <gamelani@ethz.ch>
2018-04-04 17:55:21 +0200

    Merge branches 'costanza' and 'master' of https://github.com/robertah/nlu_project into Mel
    # Conflicts:
    #	.gitignore
    #	task1/lstm.py

--- task1/lstm.py
+++ task1/lstm.py
@@ -1,26 +1,46 @@
 import tensorflow as tf
 import numpy as np
 
-# in progress by Mel --> feel free to modify
+# In progress by Mel --> feel free to modify
 
-#variables used:
+# variables used:
 # batch_size, embeddings_size
+# hl_size = hidden layer hl_size
+# z_size = (hidden layer + x) size
 
-class lstm(object):
 
-    def __init__(self, batch_size, embeddings_size):
-        self.input_x = tf.placeholder(tf.float32,
-                                      shape=(batch_size, embeddings_size), name="input_x")
+class Parameters(object):
 
-        self.b = tf.Variable(tf.zeros((embeddings_size,)), name="b")
-        self.W = tf.Variable(tf.random_uniform((batch_size, embeddings_size), -1, 1), name="W")
+    def __init__(self, hl_size, z_size):
 
-        self.C_in =
+        self.W_i = tf.Variable(tf.random_uniform((hl_size, z_size), -1, 1), name="W_i")
+        self.b_i = tf.Variable(tf.zeros((hl_size,)), name="b_i")
 
-        self.h_in =
+        self.W_f = tf.Variable(tf.random_uniform((hl_size, z_size), -1, 1), name="W_f")
+        self.b_f = tf.Variable(tf.zeros((hl_size,)), name="b_f")
 
-    def sigmoid(self, x):
-        return 1/(1 + np.exp(-x))
+        self.W_c = tf.Variable(tf.random_uniform((hl_size, z_size), -1, 1), name="W_c")
+        self.b_c = tf.Variable(tf.zeros((hl_size,)), name="b_c")
 
-    def tanh(x):
-        return np.tanh(x)
+        self.W_o = tf.Variable(tf.random_uniform((hl_size, z_size), -1, 1), name="W_o")
+        self.b_o = tf.Variable(tf.zeros((hl_size,)), name="b_o")
+
+
+def sigmoid(x):
+    return 1/(1 + np.exp(-x))
+
+
+def tanh(x):
+    return np.tanh(x)
+
+
+class Inputs():
+    def __init__(self, x_input, hl_in, C_in, batch_size, embedding_size):
+        self.x_input = tf.placeholder(tf.float32,
+                                      shape=(batch_size, embedding_size), name="x_input")
+
+        self.C_in = C_in
+
+        self.hl_in = hl_in
+
+        self.z = np.stack(self.hl_in, self.x_input)

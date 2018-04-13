e7ccf0df500d74809a96f6de03b5ba6b87113762
robertah <roberta_huang@icloud.com>
2018-04-13 22:21:44 +0200

    Added new vars in config.py

--- task1/config.py
+++ task1/config.py
@@ -1,6 +1,6 @@
-'''
+""""
 File containing configuration variables used across the project
-'''
+"""
 
 # number of the group for the project
 n_group = '20'
@@ -8,12 +8,15 @@ n_group = '20'
 # experiment
 experiment = 'A'  # 'B' or 'C'  # TODO pass the parameter with command line?
 
-# path to data sets and output files
-data_folder = '../data'  # data folder
+# path to data folder and data sets
+data_folder = '../data'
 train_set = '../data/sentences.train'
 eval_set = '../data/sentences.eval'
 cont_set = '../data/sentences.continuation'
+test_set = ''  # TODO add path to test set when we have it
 embeddings = '../data/wordembeddings-dim100.word2vec'
+
+# path to output folder
 output_folder = '/output'
 
 # token used for the language model
@@ -25,18 +28,22 @@ unk = '<unk>'  # unknown token
 # variables for the language model
 sentence_len = 30
 vocabulary_size = 20000
-batch_size = 64
-batches_per_epoch = 100
 embeddings_size = 100
+batch_size = 64
+test_batch_size = 1
+batches_per_epoch = 100
+num_epochs = 3
+lstm_cell_state = 512
+lstm_cell_state_down = 512
+
+# checkpoint
+checkpoint_every = 100
+checkpoint_dir = "./runs/1521480984/checkpoints/"
+evaluate_every = 100
 
 w2v_model_filename = "w2v_model"  # TODO not used
 dataset_filename = "input_data"  # TODO not used
 w2v_dataset_name = "wordembeddings-dim100.word2vec"  # TODO not used
 model_to_load = True
-lstm_is_training = True
-num_epochs = 3
-checkpoint_every = 100
-evaluate_every = 100
-lstm_cell_state = 512
-lstm_cell_state_down = 512
+lstm_is_training = True  # TODO need to be set at runtime?
 training_with_w2v = False

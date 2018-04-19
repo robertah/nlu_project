f9b8a3171fe80bb0008faa72bb454a1173a51cbf
roberta <roberta_huang@icloud.com>
2018-04-19 22:24:05 +0200

    Fixed config

--- task1/config.py
+++ task1/config.py
@@ -6,12 +6,15 @@ import os
 # number of the group for the project
 n_group = '20'
 
-# project task number and experiment letter
+############### SET TASK AND EXPERIMENT HERE ###############
 task = 1  # 1 or 2
 experiment = 'A'  # 'A' or 'B' or 'C'
+############################################################
+
+# take the model trained in C for task 2
+experiment = 'C' if task == 2 else experiment
 
 # max number of steps during training (number of cycles for training)
-
 # written by Francesco : note now we are going through the dataset more times so this is way higher
 # In case it is too much we can always stop before the cluster (it is 6 times the dataset the max_global_steps)
 # Note each time the data is shuffled !!! So this prevents overfitting and better training for better local
@@ -20,8 +23,8 @@ max_global_steps = 200000
 
 # run parameters
 model_to_load = True  # TODO remove? -> do not remove it yet !
-lstm_is_training = True  # change if u want to predict
-shuffle_training = True  # NEVER modify, even loclly now everything works
+lstm_is_training = True if task == 1 else False  # change if u want to predict
+shuffle_training = True  # NEVER modify, even locally now everything works
 
 # variables for the language model
 sentence_len = 30
@@ -38,7 +41,7 @@ lstm_cell_state = lstm_cell_state_down if task == 1 and experiment != 'C' else 2
 down_project = False if task == 1 and experiment != 'C' else True
 
 # path to data folder and data sets
-data_folder = '../data'
+data_folder = './data'
 train_set = data_folder + '/sentences.train'
 eval_set = data_folder + '/sentences.eval'
 cont_set = data_folder + '/sentences.continuation'
